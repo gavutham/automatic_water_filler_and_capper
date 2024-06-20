19bdb219a25d04dfea2c713a7396db8ba0a9c67d
gavutham <kgavutham2005@gmail.com>
2024-06-20 13:50:51 +0530

    for submission

--- water_filling.ino
+++ water_filling.ino
@@ -2,8 +2,8 @@ const int IR1 = 2; //detects bottle entering carrier
 const int IR2 = 4; //detects bottole entering refilling station
 const int IR3 = 6; //detects bottle entering capping station
 const int IR4 = 7; //detects capping motor reaches the bottle
-const int IR5 = 13; //for detecting whether carrier reached initial position
-
+//const int IR5 = 13; //for detecting whether carrier reached initial position
+const int buzzer=13;
 
 const int relay1 = 3; //for controlling conveyor motor
 const int relay2 = 5; //for controlling water pump
@@ -14,27 +14,36 @@ const int relay5 = 10; //for controlling gear motor and solenoid
 
 const int step = 11;
 const int direction = 12;
+const int stepper_delay = 5;
 
-int ir1_reading, ir2_reading, ir3_reading, ir4_reading, ir5_reading;
+int ir1_reading, ir2_reading, ir3_reading, ir4_reading, steps;
 
 
 void moveStepper () {
   digitalWrite(11, HIGH);
-  delayMicroseconds(500); 
+  delayMicroseconds(1000); 
   digitalWrite(step,LOW); 
-  delayMicroseconds(500); 
+  delayMicroseconds(1000); 
+}
+
+
+void beep () {
+  digitalWrite(buzzer,HIGH);
+  delay(200);
+  digitalWrite(buzzer, LOW);
 }
 
 
 void setup() {
   Serial.begin(9600); 
 
-
   pinMode(IR1, INPUT);
   pinMode(IR2, INPUT);
   pinMode(IR3, INPUT);
   pinMode(IR4, INPUT);
-  pinMode(IR5, INPUT);
+  //pinMode(IR5, INPUT);
+
+  pinMode(buzzer,OUTPUT);
 
   pinMode(relay1,OUTPUT);
   pinMode(relay2, OUTPUT);
@@ -49,13 +58,13 @@ void setup() {
   digitalWrite(relay1, HIGH);
   digitalWrite(relay2, HIGH);
   digitalWrite(relay4, HIGH);
+  digitalWrite(step, LOW);
 }
 
 void loop() {
+  steps = 0;
 
   digitalWrite(relay1, LOW); //conveyor starts
-
-  //start the conveyor motor
   Serial.println("Conveyor rotating");
 
   ir1_reading = digitalRead(IR1);
@@ -64,23 +73,23 @@ void loop() {
     delayMicroseconds(100);
   }
 
-  delay(100); //to let the bottle slide into the rotating disk
-
+  delay(500); //to let the bottle slide into the rotating disk
+  
   digitalWrite(relay1, HIGH); // conveyor stops
 
-  //stor conveyor motor (connect it to no)
   Serial.println("Bottle reached Carrier");
   Serial.println("Conveyor motor stopped");
-
-  //start stepper motor
   Serial.println("Stepper motor started rotating");
 
+  beep();
   ir2_reading = digitalRead(IR2);
   while (ir2_reading == 1) { //loop ends when bottle reaches refilling station
     ir2_reading = digitalRead(IR2);
     moveStepper();
-    delayMicroseconds(10000);
+    steps+=1;
+    delay(stepper_delay);
   }
+  beep();
 
   //stop stepper motor for fixed delay
   Serial.println("Stepper motor stopped rotating");
@@ -97,13 +106,15 @@ void loop() {
   //start stepper motor after refilling
   Serial.println("Stepper motor started rotating");
 
-
+  beep();
   ir3_reading = digitalRead(IR3);
   while (ir3_reading == 1) { //loop ends when bottle reaches capping station
     ir3_reading = digitalRead(IR3);
     moveStepper();
-    delayMicroseconds(10000);
+    steps+=1;
+    delay(stepper_delay);
   }
+  beep();
 
   Serial.println("Reached Capping Station");
   Serial.println("Stepper Motor Stops");
@@ -117,7 +128,7 @@ void loop() {
   //start air pump
   digitalWrite(relay4, LOW);
 
-  Serial.print("Air pump starts");
+  Serial.println("Air pump starts");
 
   //when ir4 senses, stop air pump, then start gear motor
   ir4_reading = digitalRead(IR4);
@@ -138,15 +149,19 @@ void loop() {
 
   //after fixed delay 
   digitalWrite(relay5, HIGH);
+
+  beep();
   Serial.println("Capping Motor stops");
+  Serial.print("Steps taken: ");
+  Serial.println(steps);
 
+  int steps_to_initial = 610-steps;
 
-  Serial.println("Stepper Motor starts");  
-  ir5_reading = digitalRead(IR5);
-  while (ir5_reading == 1) { //loop ends when carrier motor reaches near the bottle
-    ir5_reading = digitalRead(IR5);
+  for(int i=0; i<=steps_to_initial; i++) {
     moveStepper();
-    delayMicroseconds(10000);
+    delay(stepper_delay);
   }
+  Serial.println("Reached initial position");
+  beep();
 
 }
